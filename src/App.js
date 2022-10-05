ed901c2880061a03421d651c98561231296f1ae6
Rashad Muntar <muntartoure@gmail.com>
2022-10-05 23:42:22 +0000

    add chat list wrapper view

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NewChat from "./Components/newChat";
+import ChatList from "./Components/chatList";
 
 function App() {
   const coverStyle =
-    "rounded-md  w-[400px] h-[600px] bg-purple bg-gradient-to-r from-green to-blue shadow-lg shadow-gray flex";
-  return <div className={coverStyle}>
-    <NewChat />
-  </div>;
+    "rounded-md  w-[400px] h-[600px] bg-purple bg-gradient-to-r from-green to-blue shadow-lg shadow-gray flex flex-col";
+  return (
+    <div className={coverStyle}>
+       <ChatList />
+      <NewChat />
+     
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/chatList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ChatList() {
+    const styles = {
+        wrapper: 'bg-white m-[3px] w[100%] h-[93%] rounded-tl-md rounded-tr-md'
+    }
+  return (
+    <div className={styles.wrapper}>
+      
+    </div>
+  )
+}
+
+export default ChatList
