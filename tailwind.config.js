68650018ea69f0d80821e6cf9e40d4ef7de053ae
Rashad Muntar <muntartoure@gmail.com>
2022-10-05 22:11:58 +0000

    initial app set

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'green': '#13ce66',
+      'gray-light': '#d3dce6',
+    
+    },
+  },
+  plugins: [],
+}
