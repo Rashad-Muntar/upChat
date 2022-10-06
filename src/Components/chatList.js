7c0e37f0ca9466341993ccc5e7969980a241e74d
Rashad Muntar <muntartoure@gmail.com>
2022-10-06 13:30:59 +0000

    implement new user view modal

--- src/Components/chatList.js
+++ src/Components/chatList.js
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import Message from "./message";
 
+
 function ChatList() {
   const styles = {
     wrapper: "bg-white m-[3px] w[100%] h-[93%] rounded-tl-md rounded-tr-md p-[15px]",
--- src/Components/message.js
+++ src/Components/message.js
@@ -1,18 +1,31 @@
 import React from "react";
 import { FaUserCircle } from "react-icons/fa";
+import { useSelector } from "react-redux";
+
+const styles = {
+  wrapper: "flex",
+  text: `h-max bg-gradient-to-r from-green to-blue rounded-br-2xl 
+  rounded-bl-2xl text-xs rounded-tr-2xl flex p-[5px] text-white items-center`,
+  sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-full`,
+  received: `bg-[#e5e5ea] text-black float-left rounded-br-full`,
+};
 
 function Message({ name, text }) {
-  const styles = {
-    wrapper: "flex",
-    text: "h-max bg-gradient-to-r from-green to-blue rounded-br-2xl rounded-bl-2xl text-xs rounded-tr-2xl flex p-[5px] text-white items-center"
-  };
+  const currentUser = useSelector((state) => state.user);
+
+  const messageClass =
+    name === currentUser.username ? `${styles.sent}` : `${styles.received}`;
+
   return (
-    <div className={styles.wrapper}>
+    <div className={`${styles.wrapper} ${messageClass}`}>
       <div>
         <FaUserCircle fontSize="40px" />
         <small>Rashad</small>
       </div>
-      <div className={styles.text}>testing message testing message testing message testing messagetesting message testing message testing message testing message</div>
+      <div className={styles.text}>
+        testing message testing message testing message testing messagetesting
+        message testing message testing message testing message
+      </div>
     </div>
   );
 }
