911631604f096ac030eeea27339dd20e7371491d
Rashad Muntar <muntartoure@gmail.com>
2022-10-07 02:41:42 +0000

    implement test for new user and new chat

---
+++ src/newChat.test.js
@@ -0,0 +1,21 @@
+import NewChat from "./Components/newChat";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { userReducer } from "./State/userSlice";
+import { getByPlaceholderText, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+const initialState = {username: ""}
+const store = configureStore({
+    reducer: {
+        user: userReducer
+    }
+})
+
+const Wrapper = ({children}) => <Provider store={store}>{children}</Provider>
+
+test("User shoud be able to enter message", () => {
+    render(<NewChat/>, {wrapper: Wrapper});
+    userEvent.type(screen.getByPlaceholderText(/Message/i), "testing message")
+    expect(screen.getByDisplayValue("testing message")).toBeInTheDocument()
+})
